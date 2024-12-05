c6e7091ac281c52a7263b726c990109f0140c84d
Pedro Santos <pedrosantoscrlx@gmail.com>
2024-12-05 22:13:14 +0000

    WYP

--- samples/03.sh
+++ samples/03.sh
@@ -4,8 +4,9 @@ pastas=./*
 #1- rodar o script **dir_spammer.sh**
 #2- rodar esse script dentro da pasta **script_sample_folder**
 for pasta in $pastas; do
-    novo_nome="$pasta$1"
-    mv $pasta $novo_nome
+   if file in $pasta
+   rm -r $file$1
+
 done
 
 echo "tudo bene 🟢"
